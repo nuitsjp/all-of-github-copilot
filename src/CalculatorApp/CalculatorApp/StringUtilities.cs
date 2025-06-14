2164a87c43a9e0a22426cf9a50d0cd683889f5b9
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2025-06-14 12:48:57 +0900

    StringUtilitiesクラスを新規作成し、IsNullOrEmptyメソッドを追加

---
+++ src/CalculatorApp/CalculatorApp/StringUtilities.cs
@@ -0,0 +1,15 @@
+namespace UtilityLibrary
+{
+    public class StringUtilities
+    {
+        public static bool IsNullOrEmpty(string input)
+        {
+            return string.IsNullOrEmpty(input);
+        }
+        
+        // TODO: 他の文字列ユーティリティメソッドが必要
+        // - 先頭・末尾の空白除去
+        // - 大文字・小文字変換
+        // - 文字列の逆順
+    }
+}

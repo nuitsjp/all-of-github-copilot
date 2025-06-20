5199a681352f95f73f0a42427fb078c67892a1b9
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2025-06-21 07:49:40 +0900

    Calculatorクラスのドキュメントコメントを追加

--- src/CalculatorApp/CalculatorApp/Calculator.cs
+++ src/CalculatorApp/CalculatorApp/Calculator.cs
@@ -1,5 +1,23 @@
 ﻿namespace CalculatorApp;
 
+/// <summary>
+/// 四則演算を提供する電卓クラス。
+/// </summary>
 public class Calculator
 {
+    /// <summary>
+    /// 2つの整数値を加算します。
+    /// </summary>
+    /// <param name="a">1つ目の加算対象</param>
+    /// <param name="b">2つ目の加算対象</param>
+    /// <returns>加算結果</returns>
+    /// <exception cref="OverflowException">加算結果がintの範囲を超えた場合</exception>
+    public int Add(int a, int b)
+    {
+        // checkedブロックでオーバーフローを検出
+        checked
+        {
+            return a + b;
+        }
+    }
 }
